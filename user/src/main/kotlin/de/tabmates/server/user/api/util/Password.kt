4d8545fbd862adf6221e200ff0455d38e5425dec
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2026-01-14 20:56:26 +0100

    Define maximum password length to 40

--- user/src/main/kotlin/de/tabmates/server/user/api/util/Password.kt
+++ user/src/main/kotlin/de/tabmates/server/user/api/util/Password.kt
@@ -9,12 +9,15 @@ import kotlin.reflect.KClass
 @Retention(AnnotationRetention.RUNTIME)
 @Constraint(validatedBy = [])
 @Pattern(
-    regexp = "^(?=.*[\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(.{8,})$",
-    message = "Password must be at least 8 characters and contain at least one digit or special character",
+    regexp = "^(?=.*[\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(.{8,40})$",
+    message =
+        "Password must be at least 8 characters and contain at least one digit or special character. " +
+            "The maximum length is 40 characters.",
 )
 annotation class Password(
     val message: String =
-        "Password must be at least 8 characters and contain at least one digit or special character",
+        "Password must be at least 8 characters and contain at least one digit or special character. " +
+            "The maximum length is 40 characters.",
     val groups: Array<KClass<*>> = [],
     val payload: Array<KClass<out Payload>> = [],
 )
